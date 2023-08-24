8aede8d7998ae13a494c971bb7b307d108f5d8f8
Santiago Santoro <santiagosantoro10@gmail.com>
2023-08-24 13:08:04 -0300

    avances

--- src/app.js
+++ src/app.js
@@ -5,10 +5,35 @@ import { Server } from 'socket.io';
 import exphbs from 'express-handlebars';
 import productRoutes from './routes/productRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
-import  __dirname from './utils.js'; 
+import __dirname from './utils.js';
 
 const app = express();
 const server = http.createServer(app);
+
+const product = [
+  {
+    id: 1,
+    title: "Producto 1",
+    description: "Descripción del producto 1",
+    code: "code_1",
+    price: 100,
+    status: true,
+    stock: 50,
+    category: "Categoria del producto 1",
+    thumbnail: "thumbnail_1",
+  },
+  {
+    id: 2,
+    title: "Producto 2",
+    description: "Descripción del producto 2",
+    code: "code_2",
+    price: 101,
+    status: true,
+    stock: 51,
+    category: "Categoria del producto 2",
+    thumbnail: "thumbnail_2",
+  },
+];
 
 // Middleware para permitir el manejo de JSON en las solicitudes
 app.use(express.json());
@@ -25,10 +50,6 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
-// Ruta para la raíz del servidor
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido a la página principal!');
-});
 
 // Usar las rutas de productos bajo /api/products
 app.use('/api/products', productRoutes);
@@ -48,7 +69,7 @@ io.on('connection', (socket) => {
     console.log('Evento productAdded emitido:', product);
     io.emit('updateProducts', product);
   });
-  
+
 
   // Manejar el evento cuando se elimina un producto
   socket.on('productRemoved', (productId) => {
@@ -57,14 +78,17 @@ io.on('connection', (socket) => {
   });
 });
 
+// Ruta para la raíz del servidor
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido a la página principal!');
+});
 // Ruta para la vista "home"
 app.get('/home', (req, res) => {
   res.render('home');
 });
-
 // Ruta para la vista en tiempo real de productos
 app.get('/realtimeproducts', (req, res) => {
-  res.render('realTimeProducts');
+  res.render('realTimeProducts', { products: product });
 });
 
 const port = 8080;
