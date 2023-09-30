304e633789c6334a78d19c97b13f29d5d3f1846c
Santiago Santoro <santiagosantoro10@gmail.com>
2023-09-30 12:21:57 -0300

    Implementación de rol de Admin

--- src/routes/sessionsRoutes.js
+++ src/routes/sessionsRoutes.js
@@ -7,23 +7,29 @@ const router = Router();
 // Ruta para mostrar las sessions
 
 router.post('/login', async (req, res) => {
-  
     const { email, password } = req.body;
 
     const user = await usersModel.findOne({ email, password });
-    console.log(user)
-    console.log(req.session.user)
-    if (!user) return res.status(401).send({ status: "error", error: "credenciales incorrectas" });
     
+    if (!user) {
+        return res.status(401).send({ status: "error", error: "credenciales incorrectas" });
+    }
+
+    // Verifica si el correo electrónico contiene "adminCoder@coder.com" para asignar el rol de administrador
+    if (user.email.includes('adminCoder@coder.com')) {
+        user.role = 'admin'; // Asigna el rol de administrador al usuario
+    }
+
     req.session.user = {
         name: `${user.first_name} ${user.last_name}`,
         email: user.email,
-        age: user.age
+        age: user.age,
+        role: user.role || 'user' // Asigna el rol del usuario o 'user' si no se asigna un rol
     };
     
     res.send({ status: "success", payload: req.session.user });
-    
 })
+
 
 router.post('/register', async (req, res) => {
     const { first_name, last_name, email, age, password } = req.body;
