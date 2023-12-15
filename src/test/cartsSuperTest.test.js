f4a582bdfdca320db637abd12077b9c3d4b30e05
Santiago Santoro <santiagosantoro10@gmail.com>
2023-12-15 13:08:18 -0300

    Desafío Testing Avanzado

--- src/test/cartsSuperTest.test.js
+++ src/test/cartsSuperTest.test.js
@@ -36,51 +36,71 @@ describe('Testing de Carts con supertest', () => {
             expect(body.cart._id).to.equal(cartId);
         });
 
-        it('El endpoint GET /api/carts/:cartId/product/:productId debería permitir agregar un producto al carrito si el usuario está logueado', async () => {
-            const authResponse = await requester.post('/api/sessions/login')
-                .send({ email: 'santiagosantoro10@gmail.com', password: 'milo2016' });
-            const sessionCookie = authResponse.headers['set-cookie'];
-            const cartId = '656f6e2c9241ff98a3fcf92b';
-            const productId = '65031c6eec0feafb55eb7094';
-            const { statusCode, ok, body } = await requester
-                .post(`/api/carts/${cartId}/product/${productId}`)
-                .set('Cookie', sessionCookie)
-                .send({
-                    productId: '65031c6eec0feafb55eb7094',
-                    quantity: 1
-                });
+        describe('Test de productos agregados y modificados al carrito', () => {
+            it('El endpoint POST /api/carts/:cartId/product/:productId debe permitir agregar un producto al carrito si el usuario está logueado', async () => {
+                const authResponse = await requester.post('/api/sessions/login')
+                    .send({ email: 'santiagosantoro10@gmail.com', password: 'milo2016' });
+                const sessionCookie = authResponse.headers['set-cookie'];
+                const cartId = '656f6e2c9241ff98a3fcf92b';
+                const productId = '65031c6eec0feafb55eb7094';
+                const { statusCode, ok, body } = await requester
+                    .post(`/api/carts/${cartId}/product/${productId}`)
+                    .set('Cookie', sessionCookie)
+                    .send({
+                        productId: '65031c6eec0feafb55eb7094',
+                        quantity: 1
+                    });
 
-            expect(statusCode).to.equal(200);
-            expect(ok).to.be.true;
-            expect(body).to.be.an('object');
-            expect(body).to.have.property('message');
-            expect(body.message).to.equal('Producto agregado al carrito con éxito.');
+                expect(statusCode).to.equal(200);
+                expect(ok).to.be.true;
+                expect(body).to.be.an('object');
+                expect(body).to.have.property('message');
+                expect(body.message).to.equal('Producto agregado al carrito con éxito.');
+            });
+
+            it('El endpoint PUT /api/carts/:cartId/product/:productId debe permitir actualizar la cantidad de un producto en el carrito si el usuario está logueado', async () => {
+                const authResponse = await requester
+                    .post('/api/sessions/login')
+                    .send({ email: 'santiagosantoro10@gmail.com', password: 'milo2016' });
+                const sessionCookie = authResponse.headers['set-cookie'];
+                const cartId = '656f6e2c9241ff98a3fcf92b';
+                const productId = '65031c6eec0feafb55eb7094';
+                const { statusCode, ok, body } = await requester
+                    .put(`/api/carts/${cartId}/product/${productId}`)
+                    .set('Cookie', sessionCookie)
+                    .send({
+                        productId: '65031c6eec0feafb55eb7094',
+                        quantity: 3 // Nueva cantidad deseada
+                    });
+
+                expect(statusCode).to.equal(200);
+                expect(ok).to.be.true;
+                expect(body).to.be.an('object');
+                expect(body).to.have.property('message');
+                expect(body.message).to.equal('Cantidad del producto actualizada con éxito.');
+            });
+
+            it('El endpoint DELETE /api/carts/:cartId/product/:productId debe permitir eliminar un producto del carrito si el usuario está logueado', async () => {
+                const authResponse = await requester
+                    .post('/api/sessions/login')
+                    .send({ email: 'santiagosantoro10@gmail.com', password: 'milo2016' });
+                const sessionCookie = authResponse.headers['set-cookie'];
+                const cartId = '656f6e2c9241ff98a3fcf92b';
+                const productId = '65031c6eec0feafb55eb7094';
+
+                // Realiza la solicitud de eliminar producto del carrito con la cookie de sesión
+                const { statusCode, ok, body } = await requester
+                    .delete(`/api/carts/${cartId}/product/${productId}`)
+                    .set('Cookie', sessionCookie);
+
+                expect(statusCode).to.equal(200);
+                expect(ok).to.be.true;
+                expect(body).to.be.an('object');
+                expect(body).to.have.property('message');
+                expect(body.message).to.equal('Producto eliminado del carrito con éxito.');
+            });
+
         });
-
-        it('El endpoint PUT /api/carts/:cartId/product/:productId debería permitir actualizar la cantidad de un producto en el carrito si el usuario está logueado', async () => {
-            const authResponse = await requester
-                .post('/api/sessions/login')
-                .send({ email: 'santiagosantoro10@gmail.com', password: 'milo2016' });
-            const sessionCookie = authResponse.headers['set-cookie'];
-            const cartId = '656f6e2c9241ff98a3fcf92b';
-            const productId = '65031c6eec0feafb55eb7094';
-            const { statusCode, ok, body } = await requester
-                .put(`/api/carts/${cartId}/product/${productId}`)
-                .set('Cookie', sessionCookie)
-                .send({
-                    productId: '65031c6eec0feafb55eb7094',
-                    quantity: 3 // Nueva cantidad deseada
-                });
-
-            expect(statusCode).to.equal(200);
-            expect(ok).to.be.true;
-            expect(body).to.be.an('object');
-            expect(body).to.have.property('message');
-            expect(body.message).to.equal('Cantidad del producto actualizada con éxito.');
-        });
-
-
-
         after(async () => {
             await mongoose.connection.close();
         });
