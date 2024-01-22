829d023c09ac34206aae65a7e3804ddea14b9935
Santiago Santoro <santiagosantoro10@gmail.com>
2024-01-22 12:39:13 -0300

    Entrega Final

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -16,5 +16,34 @@ function finalizePurchase(cartId) {
         // Maneja el error según tus necesidades
     });
 }
+function removeProductFromCart(cartId, productId) {
+    fetch(`/api/carts/${cartId}/product/${productId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Muestra una alerta con SweetAlert indicando que se eliminó el producto del carrito
+        Swal.fire({
+          title: 'Producto eliminado',
+          text: data.message,
+          icon: 'success',
+        });
+  
+        // Recarga la página o realiza otras acciones necesarias
+        location.reload();
+      })
+      .catch(error => {
+        console.error('Error en la solicitud al servidor:', error);
+        // Muestra una alerta de error o maneja el error según tus necesidades
+        Swal.fire({
+          title: 'Error',
+          text: 'Hubo un error al eliminar el producto del carrito',
+          icon: 'error',
+        });
+      });
+  }
 
   
